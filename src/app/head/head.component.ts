96d887e130fae0a8add9a3c7eb1c0a6062596a93
olson <olson@dev.be>
2023-08-25 00:06:24 +0200

    bye links left if toggle

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-
+import { ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
   styleUrls: ['./head.component.scss']
 })
 export class HeadComponent implements OnInit {
-
+  @ViewChild('sidenav', { static: false })
+  sidenav!: MatSidenav;
   public isMenuOpen: boolean = false;
 
 
@@ -17,6 +19,7 @@ export class HeadComponent implements OnInit {
 
 
   public onSidenavClick(): void {
+    this.sidenav.toggle();
     this.isMenuOpen = false;
   }
 }
